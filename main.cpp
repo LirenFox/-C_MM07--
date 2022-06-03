0600a782cd6d0477438425afde9e11916958aaed
LirenFox <104010604+LirenFox@users.noreply.github.com>
2022-06-03 18:28:21 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int num;
+    cin >> num;
+    cout << num << " " << num*num << " " << num*num*num << endl;
+    return 0;
+}
